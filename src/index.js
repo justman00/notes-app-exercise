95c026cb2caf23e7ef1c922645417f5278f30403
Vladimir Turcan <vladimir.turcan@sumup.com>
2021-02-20 12:29:47 +0200

    .

--- src/index.js
+++ src/index.js
@@ -23,7 +23,6 @@ ReactDOM.render(
   </ChakraProvider>,
   document.getElementById('root')
 );
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
